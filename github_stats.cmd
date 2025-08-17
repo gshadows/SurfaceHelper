2aed7bd2080eaf8be079ebd44b921a1dff88ffce
G-Shadow <18046412+gshadows@users.noreply.github.com>
2025-08-17 22:14:43 +0300

    Add releases download statistics command.

---
+++ github_stats.cmd
@@ -0,0 +1,10 @@
+﻿@echo off
+
+mkdir releases 1>nul 2>nul
+pushd releases
+
+curl https://api.github.com/repos/gshadows/SurfaceHelper/releases -o releases.json
+
+type releases.json | findstr "tag_name download_count" > releases-stat.txt
+
+popd
